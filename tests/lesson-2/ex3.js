039f39ecbde548b7d1de03591fa13bea683e1de0
VietAnhDev <huuvietanh2107@gmail.com>
2025-07-21 10:43:04 +0700

    homework-2

---
+++ tests/lesson-2/ex3.js
@@ -0,0 +1,10 @@
+const a = 3;
+if (a > 0) {
+    console.log("Giá trị bạn đang nhập là số dương");
+}
+if (a < 0) {
+    console.log("Gía trị bạn đang nhập là số âm");
+}
+if (a == 0) {
+    console.log("Giá trị bạn đang nhập là số 0");
+}
---
+++ tests/lesson-2/ex4.js
@@ -0,0 +1,5 @@
+const h = 68;
+const a = h * 9 / 10;
+const b = h;
+const c = h * 8 / 10;
+console.log("cân nặng lý tưởng: " + a + "cân nặng tối đa: " + b + "cân nặng tối thiểu: " + c);
